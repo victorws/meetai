4dd82bbb0b4ed6a140684f45d085857d4e1eaf41
Victor <victorsantoso36@gmail.com>
2025-09-09 22:50:19 +0700

    18: meetings data table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-import { and, count, desc, eq, getTableColumns, ilike,} from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constant";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
@@ -70,7 +70,7 @@ export const meetingsRouter = createTRPCRouter({
     getMany: protectedProcedure
         .input(
             z.object({
-                page: z.number().default(DEFAULT_PAGE),
+                page: z.number().optional().default(DEFAULT_PAGE),
                 pageSize: z
                     .number()
                     .min(MIN_PAGE_SIZE)
@@ -85,8 +85,11 @@ export const meetingsRouter = createTRPCRouter({
             const data = await db
                 .select({
                     ...getTableColumns(meetings),
+                    agent: agents,
+                    duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
                 })
                 .from(meetings)
+                .innerJoin(agents, eq(meetings.agentId, agents.id))
                 .where(
                     and(
                         eq(meetings.userId, ctx.auth.user.id),
@@ -100,6 +103,7 @@ export const meetingsRouter = createTRPCRouter({
             const [total] = await db
                 .select({ count: count() })
                 .from(meetings)
+                .innerJoin(agents, eq(meetings.agentId, agents.id))
                 .where(
                     and(
                         eq(meetings.userId, ctx.auth.user.id),
