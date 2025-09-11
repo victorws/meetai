e16e7041eeb54f062d0071f2b0e7d5faf0beb275
Victor <victorsantoso36@gmail.com>
2025-09-12 01:32:34 +0700

    19: meetings filter

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -36,6 +36,11 @@ export const CommandSelect = ({
     const [open, setOpen] = useState(false);
     const selectedOption = options.find((option) => option.value === value);
 
+    const handleOpenChange = (open: boolean) => {
+        onSearch?.("");
+        setOpen(open);
+    };
+
     return (
         <>
             <Button
@@ -55,8 +60,8 @@ export const CommandSelect = ({
             </Button>
             <CommandResponsiveDialog
                 shouldFilter={!onSearch}
-                open={open} 
-                onOpenChange={setOpen}
+                open={open}
+                onOpenChange={handleOpenChange}
             >
                 <CommandInput placeholder="Search..." onValueChange={onSearch} />
                 <CommandList>
--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -1,13 +1,34 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
-import { NewMeetingDialog } from "./new-meeting-dialog";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { PlusIcon, XCircleIcon } from "lucide-react";
+
+import { StatusFilter } from "./status-filter";
+import { AgentIdFilter } from "./agent-id-filter";
+import { NewMeetingDialog } from "./new-meeting-dialog";
+import { MeetingsSearchFilter } from "./meetings-search-filter";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { DEFAULT_PAGE } from "@/constant";
 
 
 export const MeetingsListHeader = () => {
+  const [filters, setFilters] = useMeetingsFilters();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const isAnyFilterModified = 
+    !!filters.status || !!filters.search || !!filters.agentId;
+
+  const onClearFilters = () => {
+    setFilters({
+      status: null,
+      agentId: "",
+      search: "",
+      page: DEFAULT_PAGE,
+    });
+  };
+
   return (
     <>
       <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
@@ -19,9 +40,20 @@ export const MeetingsListHeader = () => {
               New Meeting
           </Button>
         </div>
+        <ScrollArea>
           <div className="flex items-center gap-x-2 p-1">
-            TODO: filters
+            <MeetingsSearchFilter />
+            <StatusFilter />
+            <AgentIdFilter />
+            {isAnyFilterModified && (
+              <Button variant="outline" onClick={onClearFilters}>
+                <XCircleIcon className="size-4" />
+                Clear
+              </Button>
+            )}
           </div>
+          <ScrollBar orientation="horizontal"/>
+        </ScrollArea>
       </div>
     </>
   );
